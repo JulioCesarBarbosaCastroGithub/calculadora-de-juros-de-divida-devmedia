12c8490d9696f97b47ee1c09fa4caa3fa98744a2
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2024-04-24 17:34:38 -0300

    third commit

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ console.log("\nAplicação de Juros:\n");
 
 let valorDevido = entradaDados.question("Informe o valor devido: R$ ");
 
-let diasEmAtraso = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ");
-
 let taxaDeJuros;
 
 let valorTotal;
 
-if(diasEmAtraso > 15){
-    taxaDeJuros = 10;
-    valorTotal = (valorDevido * 0.10) + Number(valorDevido);
-}else{
-    taxaDeJuros = 5;
-    valorTotal = (valorDevido * 0.05) + Number(valorDevido);
-}
+if(valorDevido > 0){
 
-console.log("\nValor original da dívida: R$ " + valorDevido);
-console.log("Dias atrasados: " + diasEmAtraso + " dias");
-console.log("Taxa de juros: " + taxaDeJuros + "%");
-console.log("Valor total com juros: R$ " + valorTotal);
+    let diasEmAtraso = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto: ");
+
+    if(diasEmAtraso > 0){
+
+            if(diasEmAtraso > 15){
+                taxaDeJuros = 10;
+                valorTotal = (valorDevido * 0.10) + Number(valorDevido);
+            }else{
+                taxaDeJuros = 5;
+                valorTotal = (valorDevido * 0.05) + Number(valorDevido);
+            }
+
+            console.log("\nValor original da dívida: R$ " + valorDevido);
+            console.log("Dias atrasados: " + diasEmAtraso + " dias");
+            console.log("Taxa de juros: " + taxaDeJuros + "%");
+            console.log("Valor total com juros: R$ " + valorTotal);
+    }else{
+        console.log("A dívida deve ter pelo menos 1 dia de atraso!");
+    }
+}else{
+    console.log("O valor da dívida deve ser maior que zero!");
+}
